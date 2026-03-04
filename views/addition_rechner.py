7f841305876bf9a6e9b8f580e9cee38fb5d1a138
caan <gomesana@students.zhaw.ch>
2026-03-04 12:20:51 +0100

    Rechner

---
+++ views/addition_rechner.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from functions.addition import add
+
+st.title("Addition Rechner.")
+
+st.write("Hier ist mein Addition Rechner. Er addiert zwei Zahlen.")
+
+with st.form("Additions_form")
+    number1 = st.number_input("Number 1")
+    number2 = st.number_input("Number 2")
+
+button = st.form_submit_button()
+
+if button:
+    result = add(number1, number2)
+    st.write(f"Das Ergebnis ist: {result}")
