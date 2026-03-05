af7a5d6b47e4555046614ad0098ea21617115556
caan <gomesana@students.zhaw.ch>
2026-03-05 11:20:21 +0100

    Neu

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-import views.kalorien_rechner as kalorien_rechner
+import streamlit as st
+
+pg_home = st.Page("views/home.py", title="Home")
+pg_kalorien = st.Page("views/kalorien_rechner.py", title="Kalorienbedarf-Rechner")
+
+pg = st.navigation([pg_home, pg_kalorien])
+pg.run()
